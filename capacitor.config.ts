0e07e1341d9ad41c063929e1a3a63a4e0d3b05a6
Aaron Mena <aamenabo@est.utn.ac.cr>
2025-04-09 17:48:38 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EX02_qr_reader',
+  webDir: 'dist'
+};
+
+export default config;
